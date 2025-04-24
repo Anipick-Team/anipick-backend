368a40324460b5c82b282fb2d76628370d2a84f0
yuuzinn <s110528@nate.com>
2025-04-24 14:25:39 +0900

    Initial commit

---
+++ src/main/java/com/anipick/backend/AnipickBackendApplication.java
@@ -0,0 +1,13 @@
+package com.anipick.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnipickBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnipickBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anipick/backend/AnipickBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anipick.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnipickBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
