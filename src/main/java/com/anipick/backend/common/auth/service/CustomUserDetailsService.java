c208d7481f29aa4d5daae903e8f8aa285f93b8ae
Woongineering <chldbfkr456@naver.com>
2025-05-07 23:55:32 +0900

    [#17] refactor/CustomException 적용

--- src/main/java/com/anipick/backend/common/auth/service/CustomUserDetailsService.java
+++ src/main/java/com/anipick/backend/common/auth/service/CustomUserDetailsService.java
@@ -1,6 +1,8 @@
 package com.anipick.backend.common.auth.service;
 
 import com.anipick.backend.common.auth.dto.CustomUserDetails;
+import com.anipick.backend.common.exception.CustomException;
+import com.anipick.backend.common.exception.ErrorCode;
 import com.anipick.backend.user.domain.User;
 import com.anipick.backend.user.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
@@ -17,7 +19,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userMapper.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+                .orElseThrow(() -> new CustomException(ErrorCode.ACCOUNT_NOT_FOUND_BY_EMAIL));
         return new CustomUserDetails(user);
     }
 }
