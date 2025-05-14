e0709bbd38f9fc59eaf483d7fe197fd04b95214a
Woongineering <chldbfkr456@naver.com>
2025-05-15 01:26:36 +0900

    [#33] refactor/refreshToken 필드 삭제

--- src/main/java/com/anipick/backend/user/domain/UserDefaults.java
+++ src/main/java/com/anipick/backend/user/domain/UserDefaults.java
@@ -8,5 +8,4 @@ public final class UserDefaults {
     public static final String DEFAULT_PROFILE_IMAGE_URL = "default.png";
     public static final Boolean DEFAULT_ADULT_YN = false;
     public static final Boolean DEFAULT_REVIEW_COMPLETED_YN = false;
-    public static final Integer DEFAULT_REFRESH_TOKEN_EXPIRATION_DAYS = 14;
 }
