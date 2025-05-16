5e874648e5261062c8b609b2cc641ff123c0c738
Woongineering <chldbfkr456@naver.com>
2025-05-17 00:15:02 +0900

    [#33] fix/반환 타입 void 적용

--- src/main/java/com/anipick/backend/user/controller/UserController.java
+++ src/main/java/com/anipick/backend/user/controller/UserController.java
@@ -3,7 +3,6 @@ package com.anipick.backend.user.controller;
 import com.anipick.backend.common.dto.ApiResponse;
 import com.anipick.backend.token.dto.TokenResponse;
 import com.anipick.backend.user.dto.LoginRequest;
-import com.anipick.backend.user.domain.User;
 import com.anipick.backend.user.dto.SignUpRequest;
 import com.anipick.backend.user.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -16,9 +15,9 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public ApiResponse<User> signUp(@RequestBody SignUpRequest request) {
-        User user = userService.signUp(request);
-        return ApiResponse.success(user);
+    public ApiResponse<Void> signUp(@RequestBody SignUpRequest request) {
+        userService.signUp(request);
+        return ApiResponse.success();
     }
 
     @PostMapping("/login")
