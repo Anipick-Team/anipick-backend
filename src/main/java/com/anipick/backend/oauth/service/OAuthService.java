b794cacee3150957e528e718762e662138c5e5af
Woongineering <chldbfkr456@naver.com>
2025-05-22 01:35:24 +0900

    [#60] feat/google 소셜 로그인 추가

---
+++ src/main/java/com/anipick/backend/oauth/service/OAuthService.java
@@ -0,0 +1,79 @@
+package com.anipick.backend.oauth.service;
+
+import com.anipick.backend.common.exception.CustomException;
+import com.anipick.backend.common.exception.ErrorCode;
+import com.anipick.backend.oauth.dto.SocialLoginRequest;
+import com.anipick.backend.oauth.util.GoogleVerifierUtil;
+import com.anipick.backend.token.dto.TokenResponse;
+import com.anipick.backend.token.service.TokenService;
+import com.anipick.backend.user.domain.LoginFormat;
+import com.anipick.backend.user.domain.User;
+import com.anipick.backend.user.domain.UserDefaults;
+import com.anipick.backend.user.mapper.UserMapper;
+import com.anipick.backend.user.service.NicknameInitializer;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class OAuthService {
+    private final UserMapper userMapper;
+    private final TokenService tokenService;
+
+    public TokenResponse socialLogin(SocialLoginRequest request, String provider) {
+        String platform = request.getPlatform();
+        String idToken = request.getCode();
+
+        switch (provider) {
+            case "GOOGLE":
+                return googleLogin(platform, idToken);
+        }
+
+        return null;
+    }
+
+    private TokenResponse googleLogin(String platform, String idToken) {
+        try {
+            GoogleIdToken.Payload payload;
+            if(platform.equals("android")) {
+                payload = GoogleVerifierUtil.verifyAndroidGoogleToken(idToken);
+            } else if(platform.equals("ios")) {
+                payload = GoogleVerifierUtil.verifyIosGoogleToken(idToken);
+            } else {
+                throw new CustomException(ErrorCode.BAD_REQUEST);
+            }
+
+            String email = payload.getEmail();
+            if(checkExistsEmail(email)) {
+                return tokenService.generateAndSaveTokens(email);
+            } else {
+                String nickname = NicknameInitializer.generateUniqueNickname(userMapper::existsByNickname);
+                User user = User.builder()
+                        .email(email)
+                        .nickname(nickname)
+                        .loginFormat(LoginFormat.GOOGLE)
+                        .profileImageUrl(UserDefaults.DEFAULT_PROFILE_IMAGE_URL)
+                        .termsAndConditions(true)
+                        .adultYn(UserDefaults.DEFAULT_ADULT_YN)
+                        .reviewCompletedYn(UserDefaults.DEFAULT_REVIEW_COMPLETED_YN)
+                        .createdAt(LocalDateTime.now())
+                        .updatedAt(LocalDateTime.now())
+                        .build();
+
+                userMapper.insertUser(user);
+            }
+
+        } catch (Exception e) {
+            throw new CustomException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+
+        return null;
+    }
+
+    private boolean checkExistsEmail(String email) {
+        return userMapper.findByEmail(email).isPresent();
+    }
+}
---
+++ src/main/java/com/anipick/backend/oauth/util/GoogleVerifierUtil.java
@@ -0,0 +1,44 @@
+package com.anipick.backend.oauth.util;
+
+import com.anipick.backend.common.exception.CustomException;
+import com.anipick.backend.common.exception.ErrorCode;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+public class GoogleVerifierUtil {
+    @Value("${app.oauth2.google.android-client-id}")
+    private static String androidClientId;
+
+    @Value("${app.oauth2.google.ios-client-id}")
+    private static String iosClientId;
+
+    public static GoogleIdToken.Payload verifyAndroidGoogleToken(String token) throws GeneralSecurityException, IOException {
+        return getPayload(token, androidClientId);
+    }
+
+    public static GoogleIdToken.Payload verifyIosGoogleToken(String token) throws GeneralSecurityException, IOException {
+        return getPayload(token, iosClientId);
+    }
+
+    private static GoogleIdToken.Payload getPayload(String token, String clientId) throws GeneralSecurityException, IOException {
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
+                .setAudience(Collections.singletonList(clientId))
+                .build();
+
+        GoogleIdToken googleIdToken = verifier.verify(token);
+        if(googleIdToken != null) {
+            return googleIdToken.getPayload();
+        } else {
+            throw new CustomException(ErrorCode.REQUESTED_TOKEN_INVALID);
+        }
+    }
+}
