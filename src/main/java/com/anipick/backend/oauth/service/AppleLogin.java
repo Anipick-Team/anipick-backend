3fa3a80221413d28c5359afb0accdc71d8c62d38
Woongineering <chldbfkr456@naver.com>
2025-05-30 00:59:58 +0900

    [#61] feat/애플 로그인 추가

---
+++ src/main/java/com/anipick/backend/oauth/service/AppleLogin.java
@@ -0,0 +1,37 @@
+package com.anipick.backend.oauth.service;
+
+import com.anipick.backend.common.exception.CustomException;
+import com.anipick.backend.common.exception.ErrorCode;
+import com.anipick.backend.oauth.domain.AppleDefaults;
+import com.anipick.backend.oauth.domain.Provider;
+import com.anipick.backend.oauth.dto.SocialLoginRequest;
+import com.anipick.backend.oauth.util.CommonLoginUtil;
+import com.anipick.backend.token.dto.TokenResponse;
+import com.anipick.backend.user.domain.LoginFormat;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AppleLogin implements SocialLogin {
+    private final CommonLoginUtil commonLoginUtil;
+
+    @Override
+    public boolean checkProvider(Provider provider) {
+        return provider == Provider.APPLE;
+    }
+
+    @Override
+    public TokenResponse login(SocialLoginRequest request) {
+        String prefixEmail = request.getCode();
+
+        try {
+            String email = AppleDefaults.DEFAULT_APPLE_EMAIL_FORMAT
+                    .formatted(prefixEmail);
+            return commonLoginUtil.signUpAndLogin(email, LoginFormat.APPLE);
+
+        } catch (Exception e) {
+            throw new CustomException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
