796bebb42c5a9908c7ab23fbb5bb58321b5833b0
yuuzinn <s110528@nate.com>
2025-05-29 20:52:29 +0900

    [#64] feat/평가 작성 시 유저 시청 상태 반영

--- src/main/java/com/anipick/backend/review/service/RatingService.java
+++ src/main/java/com/anipick/backend/review/service/RatingService.java
@@ -4,6 +4,9 @@ import com.anipick.backend.common.exception.CustomException;
 import com.anipick.backend.common.exception.ErrorCode;
 import com.anipick.backend.review.dto.ReviewRatingRequest;
 import com.anipick.backend.review.mapper.RatingMapper;
+import com.anipick.backend.user.domain.UserAnimeOfStatus;
+import com.anipick.backend.user.domain.UserAnimeStatus;
+import com.anipick.backend.user.mapper.UserAnimeStatusMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class RatingService {
 
     private final RatingMapper ratingMapper;
+    private final UserAnimeStatusMapper userAnimeStatusMapper;
 
     @Transactional
     public void createReviewRating(Long animeId, ReviewRatingRequest request, Long userId) {
@@ -21,6 +25,17 @@ public class RatingService {
             throw new CustomException(ErrorCode.REVIEW_ALREADY_EXISTS);
         }
         ratingMapper.createReviewRating(animeId, userId, request);
+
+        UserAnimeStatus userAnimeStatus = userAnimeStatusMapper.findByUserId(userId, animeId);
+        if (userAnimeStatus != null) {
+            UserAnimeOfStatus status = userAnimeStatus.getStatus();
+            String statusName = status.name();
+            if (!statusName.equals("FINISHED")) {
+                userAnimeStatusMapper.updateUserAnimeStatus(userId, animeId, UserAnimeOfStatus.FINISHED);
+            }
+        } else {
+            userAnimeStatusMapper.createUserAnimeStatus(userId, animeId, UserAnimeOfStatus.FINISHED);
+        }
     }
 
     @Transactional
