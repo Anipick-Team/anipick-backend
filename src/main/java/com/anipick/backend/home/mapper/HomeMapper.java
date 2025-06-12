9da02c5e1567f6d8b95ccf3d2bacaa3eeec1c9db
yuuzinn <s110528@nate.com>
2025-06-12 12:59:41 +0900

    [#99] feat/홈 - 최근 리뷰, 공개예정 조회

---
+++ src/main/java/com/anipick/backend/home/mapper/HomeMapper.java
@@ -0,0 +1,23 @@
+package com.anipick.backend.home.mapper;
+
+import com.anipick.backend.anime.dto.ComingSoonItemBasicDto;
+import com.anipick.backend.home.dto.HomeComingSoonItemDto;
+import com.anipick.backend.home.dto.HomeRecentReviewItemDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface HomeMapper {
+    List<HomeRecentReviewItemDto> selectHomeRecentReviews(
+            @Param(value = "currentUserId") Long userId,
+            @Param(value = "size") int size
+    );
+
+    List<ComingSoonItemBasicDto> selectHomeComingSoonAnimes(
+            @Param(value = "defaultCoverUrl") String defaultCoverUrl,
+            @Param(value = "orderByQuery") String orderByQuery,
+            @Param(value = "size") int size
+    );
+}
