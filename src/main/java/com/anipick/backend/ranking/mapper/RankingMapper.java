2bd995d368d165d467c500081756168f3f73d9e9
ChoiSeoWoong <chldbfkr456@naver.com>
2025-06-14 20:14:18 +0900

    [#98] feat/랭킹 매퍼 추가

--- src/main/java/com/anipick/backend/ranking/mapper/RankingMapper.java
+++ src/main/java/com/anipick/backend/ranking/mapper/RankingMapper.java
@@ -1,7 +1,41 @@
 package com.anipick.backend.ranking.mapper;
 
+import com.anipick.backend.anime.dto.GenreDto;
+import com.anipick.backend.ranking.dto.RankingAnimesFromQueryDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Mapper
 public interface RankingMapper {
+    List<RankingAnimesFromQueryDto> getYearSeasonRankingFromNotFilter(
+            @Param("year") Integer year,
+            @Param("season") Integer season,
+            @Param("rankDate") LocalDate rankDate
+    );
+
+    List<RankingAnimesFromQueryDto> getYearSeasonRankingFromFilter(
+            @Param("year") Integer year,
+            @Param("season") Integer season,
+            @Param("genre") String genre,
+            @Param("rankDate") LocalDate rankDate
+    );
+
+    List<RankingAnimesFromQueryDto> getYearSeasonRanking(
+            @Param("year") Integer year,
+            @Param("season") Integer season,
+            @Param("genre") String genre,
+            @Param("lastId") Long lastId,
+            @Param("size") Integer size,
+            @Param("rankDate") LocalDate rankDate
+    );
+
+    List<RankingAnimesFromQueryDto> getAllTimeRanking(
+            @Param("genre") String genre,
+            @Param("lastId") Long lastId,
+            @Param("size") Integer size,
+            @Param("rankDate") LocalDate rankDate
+    );
 }
