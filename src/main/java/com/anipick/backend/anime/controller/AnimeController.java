4374f15993a7bfcd522d7a6990b38d7ae5a7c5e0
yuuzinn <s110528@nate.com>
2025-06-17 16:43:50 +0900

    [#114] feat/애니 상세 (작품) - 함께 볼만한 작품 조회

--- src/main/java/com/anipick/backend/anime/controller/AnimeController.java
+++ src/main/java/com/anipick/backend/anime/controller/AnimeController.java
@@ -1,5 +1,6 @@
 package com.anipick.backend.anime.controller;
 
+import com.anipick.backend.anime.dto.AnimeItemDto;
 import com.anipick.backend.anime.dto.ComingSoonPageDto;
 import com.anipick.backend.anime.dto.UpcomingSeasonResultDto;
 import com.anipick.backend.anime.service.AnimeService;
@@ -7,10 +8,9 @@ import com.anipick.backend.common.auth.dto.CustomUserDetails;
 import com.anipick.backend.common.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/animes")
@@ -39,6 +39,15 @@ public class AnimeController {
 		ComingSoonPageDto result = animeService.getComingSoonAnimes(
 				sort, lastId, size, includeAdult, lastValue
 		);
+		return ApiResponse.success(result);
+	}
+
+	@GetMapping("/{animeId}/detail/recommendation")
+	public ApiResponse<List<AnimeItemDto>> getAnimeDetailRecommendation(
+			@PathVariable(value = "animeId") Long animeId,
+			@AuthenticationPrincipal CustomUserDetails user
+	) {
+		List<AnimeItemDto> result = animeService.getAnimeRecommendation(animeId);
 		return ApiResponse.success(result);
 	}
 }
