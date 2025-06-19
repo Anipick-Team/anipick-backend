2a3997b521f1fafb58d36d93b536e31f10abf544
Woongineering <chldbfkr456@naver.com>
2025-06-20 00:17:41 +0900

    [#101] feat/마이페이지 조회 쿼리 추가

--- src/main/java/com/anipick/backend/mypage/mapper/MyPageMapper.java
+++ src/main/java/com/anipick/backend/mypage/mapper/MyPageMapper.java
@@ -1,7 +1,17 @@
 package com.anipick.backend.mypage.mapper;
 
+import com.anipick.backend.mypage.dto.LikedAnimesDto;
+import com.anipick.backend.mypage.dto.LikedPersonsDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface MyPageMapper {
+    Long getMyWatchCount(@Param("userId") Long userId, @Param("animeStatus") String animeStatus);
+
+    List<LikedAnimesDto> getMyLikedAnimes(@Param("userId") Long userId, @Param("size") Integer size);
+
+    List<LikedPersonsDto> getMyLikedPersons(@Param("userId") Long userId, @Param("size") Integer size);
 }
