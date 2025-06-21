300a9c14ac3e9a8af8b717b3abb58ae50fb77898
Woongineering <chldbfkr456@naver.com>
2025-06-21 16:19:04 +0900

    [#102] feat/마이페이지 시청 기록 로직 추가

--- src/main/java/com/anipick/backend/mypage/mapper/MyPageMapper.java
+++ src/main/java/com/anipick/backend/mypage/mapper/MyPageMapper.java
@@ -2,6 +2,8 @@ package com.anipick.backend.mypage.mapper;
 
 import com.anipick.backend.mypage.dto.LikedAnimesDto;
 import com.anipick.backend.mypage.dto.LikedPersonsDto;
+import com.anipick.backend.mypage.dto.WatchListAnimesDto;
+import com.anipick.backend.mypage.dto.WatchingAnimesDto;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -14,4 +16,22 @@ public interface MyPageMapper {
     List<LikedAnimesDto> getMyLikedAnimes(@Param("userId") Long userId, @Param("size") Integer size);
 
     List<LikedPersonsDto> getMyLikedPersons(@Param("userId") Long userId, @Param("size") Integer size);
+
+    List<WatchListAnimesDto> getMyWatchListAnimes(
+            @Param("userId") Long userId,
+            @Param("status") String status,
+            @Param("lastId") Long lastId,
+            @Param("size") Integer size
+    );
+
+    Long getMyWatchListAnimesCount(@Param("userId") Long userId, @Param("status") String status);
+
+    List<WatchingAnimesDto> getMyWatchingAnimes(
+            @Param("userId") Long userId,
+            @Param("status") String status,
+            @Param("lastId") Long lastId,
+            @Param("size") Integer size
+    );
+
+    Long getMyWatchingAnimesCount(@Param("userId") Long userId, @Param("status") String status);
 }
