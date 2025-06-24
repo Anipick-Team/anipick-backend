28c12cdd2b7ca7330522cadcf264b27f6fa346b8
ChoiSeoWoong <chldbfkr456@naver.com>
2025-06-24 21:10:50 +0900

    [#102] feat/다 본 애니 dto 추가

---
+++ src/main/java/com/anipick/backend/mypage/dto/FinishedAnimesDto.java
@@ -0,0 +1,15 @@
+package com.anipick.backend.mypage.dto;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class FinishedAnimesDto {
+    private Long animeId;
+    private Long userAnimeStatusId;
+    private String title;
+    private String coverImageUrl;
+    private Double myRating;
+    private LocalDateTime createdAt;
+}
