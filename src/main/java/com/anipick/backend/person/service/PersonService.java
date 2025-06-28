06e19fa7e02f90869d61b64bbbdce1286f3f2efa
yuuzinn <s110528@nate.com>
2025-06-28 19:18:30 +0900

    [#137] feat/성우 인물 상세 조회

--- src/main/java/com/anipick/backend/person/service/PersonService.java
+++ src/main/java/com/anipick/backend/person/service/PersonService.java
@@ -17,21 +17,20 @@ public class PersonService {
     private final PersonMapper personMapper;
 
     public PersonDetailPageDto getAnimeAndCharacter(Long personId, Long lastId, int size, Long userId) {
-        // TODO : 성우의 정보 가져오기 (ID, NAME, PROFILE_URL, USER_IS_LIKED) - 파라미터 personId, userId
         PersonInfoDto personInfoDto = personMapper.selectActorInfo(personId, userId);
-        // TODO : 성우가 참여한 애니들 개수 가져오기 - 파라미터 personId
+
         long totalCount = personMapper.countAnimesByActor(personId);
-        // TODO : 성우가 참여한 애니 리스트 가져오기 - 파라미터 personId, lastId, size
+
         List<PersonAnimeWorkDto> items = personMapper.selectWorksByActor(personId, lastId, size);
-        // TODO : Cursor 객체 조립 후 PersonDetailPageDto 객체 조립 -> 리턴
+
         Long nextId;
         if (items.isEmpty()) {
             nextId = null;
         } else {
             nextId = items.getLast().getAnimeId();
         }
-        CursorDto cursor = CursorDto.of(lastId);
-        // TODO : 조립
+        CursorDto cursor = CursorDto.of(nextId);
+
         return PersonDetailPageDto.of(
                 personInfoDto.getPersonId(),
                 personInfoDto.getName(),
