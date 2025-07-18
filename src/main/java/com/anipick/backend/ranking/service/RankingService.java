0d8e2d36fa633cc77692337342dfccf831dca399
Woongineering <chldbfkr456@naver.com>
2025-07-18 23:23:33 +0900

    [#157] fix/연관 장르 조건 추가, 특정 장르 기준 정렬 추가

--- src/main/java/com/anipick/backend/ranking/service/RankingService.java
+++ src/main/java/com/anipick/backend/ranking/service/RankingService.java
@@ -1,5 +1,6 @@
 package com.anipick.backend.ranking.service;
 
+import com.anipick.backend.anime.dto.GenreDto;
 import com.anipick.backend.common.dto.CursorDto;
 import com.anipick.backend.common.exception.CustomException;
 import com.anipick.backend.common.exception.ErrorCode;
@@ -12,6 +13,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +25,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class RankingService {
     private final RankingMapper rankingMapper;
     private final RealTimeRankingMapper realTimeRankingMapper;
@@ -77,14 +80,20 @@ public class RankingService {
 
         List<RankingAnimesFromQueryDto> yearSeasonRankingToday = rankingMapper.getYearSeasonRankingPaging(year, season, genre, lastId, size, today);
         List<RankingAnimesFromQueryDto> yearSeasonRankingYesterday = rankingMapper.getYearSeasonRanking(year, season, genre, yesterday);
+        List<Long> animeIds = yearSeasonRankingToday.stream()
+                .map(RankingAnimesFromQueryDto::getAnimeId)
+                .toList();
 
+        List<AnimeGenresDto> genresByAnimeIds = rankingMapper.getGenresByAnimeIds(animeIds);
+        Map<Long, List<AnimeGenresDto>> animeGenresMap = genresByAnimeIds.stream()
+                .collect(Collectors.groupingBy(AnimeGenresDto::getAnimeId));
         Map<Long, Long> yesterdayRankMap =  yearSeasonRankingYesterday.stream()
                 .collect(Collectors.toMap(
                         RankingAnimesFromQueryDto::getAnimeId,
                         RankingAnimesFromQueryDto::getRank
                 ));
-
         List<RankingAnimesDto> animes;
+        Long newLastId;
         AtomicReference<Long> displayRank = new AtomicReference<>(0L);
 
         if(genre != null) {
@@ -93,23 +102,27 @@ public class RankingService {
                         Long todayRank = dto.getRank();
                         Long yesterdayRank = yesterdayRankMap.get(dto.getAnimeId());
                         displayRank.getAndSet(displayRank.get() + 1);
+                        List<AnimeGenresDto> genres = animeGenresMap.get(dto.getAnimeId());
 
-                        return calcAndMakeAnimeRanking(todayRank, yesterdayRank, displayRank.get(), dto);
+                        return calcAndMakeAnimeRanking(todayRank, yesterdayRank, displayRank.get(), dto, genres, genre);
                     })
                     .toList();
+            newLastId = animes.getLast().getRank();
         } else {
             animes = yearSeasonRankingToday.stream()
                     .map(dto -> {
                         Long todayRank = dto.getRank();
                         Long yesterdayRank = yesterdayRankMap.get(dto.getAnimeId());
                         displayRank.set(todayRank);
+                        List<AnimeGenresDto> genres = animeGenresMap.get(dto.getAnimeId());
 
-                        return calcAndMakeAnimeRanking(todayRank, yesterdayRank, displayRank.get(), dto);
+                        return calcAndMakeAnimeRanking(todayRank, yesterdayRank, displayRank.get(), dto, genres, genre);
                     })
                     .toList();
+            newLastId = animes.getLast().getRank();
         }
 
-        CursorDto cursor = CursorDto.of(lastId);
+        CursorDto cursor = CursorDto.of(newLastId);
 
         return RankingResponse.of(cursor, animes);
     }
@@ -120,11 +133,19 @@ public class RankingService {
 
         List<RankingAnimesFromQueryDto> allTimeRankingToday = rankingMapper.getAllTimeRankingPaging(genre, lastId, size, today);
         List<RankingAnimesFromQueryDto> allTimeRankingYesterday = rankingMapper.getAllTimeRanking(genre, yesterday);
+        List<Long> animeIds = allTimeRankingToday.stream()
+                .map(RankingAnimesFromQueryDto::getAnimeId)
+                .toList();
+
+        List<AnimeGenresDto> genresByAnimeIds = rankingMapper.getGenresByAnimeIds(animeIds);
+        Map<Long, List<AnimeGenresDto>> animeGenresMap = genresByAnimeIds.stream()
+                .collect(Collectors.groupingBy(AnimeGenresDto::getAnimeId));
 
         Map<Long, Long> yesterdayRankMap = allTimeRankingYesterday.stream()
                 .collect(Collectors.toMap(RankingAnimesFromQueryDto::getAnimeId, RankingAnimesFromQueryDto::getRank));
 
         List<RankingAnimesDto> animes;
+        Long newLastId;
         AtomicReference<Long> displayRank = new AtomicReference<>(0L);
 
         if(genre != null) { // 장르가 있을 때는 순위를 재정렬해서 보여줌
@@ -133,28 +154,32 @@ public class RankingService {
                         Long todayRank = dto.getRank();
                         Long yesterdayRank = yesterdayRankMap.get(dto.getAnimeId());
                         displayRank.getAndSet(displayRank.get() + 1);
+                        List<AnimeGenresDto> genres = animeGenresMap.get(dto.getAnimeId());
 
-                        return calcAndMakeAnimeRanking(todayRank, yesterdayRank, displayRank.get(), dto);
+                        return calcAndMakeAnimeRanking(todayRank, yesterdayRank, displayRank.get(), dto, genres, genre);
                     })
                     .toList();
+            newLastId = animes.getLast().getRank();
         } else { // 장르가 없을 때는 DB 순위를 그대로 가져와서 보여줌
             animes = allTimeRankingToday.stream()
                     .map(dto -> {
                         Long todayRank = dto.getRank();
                         Long yesterdayRank = yesterdayRankMap.get(dto.getAnimeId());
                         displayRank.set(todayRank);
+                        List<AnimeGenresDto> genres = animeGenresMap.get(dto.getAnimeId());
 
-                        return calcAndMakeAnimeRanking(todayRank, yesterdayRank, displayRank.get(), dto);
+                        return calcAndMakeAnimeRanking(todayRank, yesterdayRank, displayRank.get(), dto, genres, genre);
                     })
                     .toList();
+            newLastId = animes.getLast().getRank();
         }
 
-        CursorDto cursor = CursorDto.of(lastId);
+        CursorDto cursor = CursorDto.of(newLastId);
 
         return RankingResponse.of(cursor, animes);
     }
 
-    private RankingAnimesDto calcAndMakeAnimeRanking(Long todayRank, Long yesterdayRank, Long displayRank, RankingAnimesFromQueryDto dto) {
+    private RankingAnimesDto calcAndMakeAnimeRanking(Long todayRank, Long yesterdayRank, Long displayRank, RankingAnimesFromQueryDto dto, List<AnimeGenresDto> genres, String specificGenre) {
         long change;
         Trend trend;
 
@@ -174,6 +199,23 @@ public class RankingService {
             }
         }
 
-        return RankingAnimesDto.from(change, trend, displayRank, dto);
+        List<String> genreNames = genres.stream()
+                .map(AnimeGenresDto::getGenreName)
+                .collect(Collectors.toList());
+
+        if(specificGenre != null && genreNames.contains(specificGenre)) {
+            genreNames.sort((genre1, genre2) -> {
+                if(genre1.equals(specificGenre)) {
+                    return -1;
+                }
+                if(genre2.equals(specificGenre)) {
+                    return 1;
+                }
+
+                return genre1.compareTo(genre2);
+            });
+        }
+
+        return RankingAnimesDto.from(change, trend, displayRank, dto, genreNames);
     }
 }
