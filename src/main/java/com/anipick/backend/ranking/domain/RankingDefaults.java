69467680aa0b0d320d401c3feb490f59127da343
Woongineering <chldbfkr456@naver.com>
2025-07-18 23:28:47 +0900

    [#157] fix/MAX_YEAR, MIN_YEAR 필드 삭제

--- src/main/java/com/anipick/backend/ranking/domain/RankingDefaults.java
+++ src/main/java/com/anipick/backend/ranking/domain/RankingDefaults.java
@@ -6,7 +6,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RankingDefaults {
     public static final String RANKING_ALIAS_KEY = "anime_real_rank:";
-    public static final int MIN_YEAR = 1940;
-    public static final int MAX_YEAR = 2026;
     public static final int ONE_DAY = 1;
 }
