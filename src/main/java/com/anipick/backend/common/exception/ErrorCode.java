15fdc1be89fac74958f3ad4943ed0e7bf6419d83
Woongineering <chldbfkr456@naver.com>
2025-08-04 22:11:35 +0900

    [#208] feat/IMAGE_DATA_NOT_FOUND 에러 추가

--- src/main/java/com/anipick/backend/common/exception/ErrorCode.java
+++ src/main/java/com/anipick/backend/common/exception/ErrorCode.java
@@ -36,7 +36,7 @@ public enum ErrorCode {
 	EMAIL_PREPARE_ERROR(125, "메시지 준비 오류", "메시지 준비 중 오류가 발생하였습니다."),
 	EMAIL_SEND_FAILED(126, "메일 전송 실패", "메일 전송이 실패하였습니다."),
 	EMAIL_GENERIC_ERROR(127, "일반적인 메일 오류", "일반적인 메일 오류가 발생하였습니다."),
-  ALREADY_USER_ANIME_OF_STATUS(128, "유저 시청상태 데이터가 이미 존재", null),
+  	ALREADY_USER_ANIME_OF_STATUS(128, "유저 시청상태 데이터가 이미 존재", null),
 	USER_ANIME_OF_STATUS_DATA_NOT_FOUND(129, "유저 시청상태 데이터 찾을 수 없음", null),
 	ALREADY_BLOCKED_USER(130, "이미 차단한 유저", null),
 	SELF_BLOCKED_USER_ERROR(131, "본인 자신은 차단 불가", null),
@@ -73,7 +73,8 @@ public enum ErrorCode {
 	/**
 	 * Image
 	 */
-	INVAILD_IMAGE_EXTENSION(901, "유효한 이미지 확장자가 아님", null);
+	INVAILD_IMAGE_EXTENSION(901, "유효한 이미지 확장자가 아님", null),
+	IMAGE_DATA_NOT_FOUND(902, "이미지 데이터 찾을 수 없음", null);
 
 	private final int code;
 	private final String errorReason;
