a5fd36f9eae1ba7be1b25cc6c979a1d6e546dc28
yuuzinn <s110528@nate.com>
2025-08-10 15:10:14 +0900

    [#216] fix/StringUtils를 활용한 문자열 빈값 확인

--- src/main/java/com/anipick/backend/review/dto/ReviewReportMessageRequest.java
+++ src/main/java/com/anipick/backend/review/dto/ReviewReportMessageRequest.java
@@ -3,13 +3,14 @@ package com.anipick.backend.review.dto;
 import com.anipick.backend.common.exception.CustomException;
 import com.anipick.backend.common.exception.ErrorCode;
 import lombok.Getter;
+import org.springframework.util.StringUtils;
 
 @Getter
 public class ReviewReportMessageRequest {
     private String message;
 
     public void validate() {
-        if (message == null || message.trim().isEmpty()) {
+        if (!StringUtils.hasText(message)) {
             throw new CustomException(ErrorCode.REPORT_MESSAGE_EMPTY);
         }
     }
