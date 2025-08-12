d44220386d6867161924839201785d04cabbbb2d
ChoiSeoWoong <chldbfkr456@naver.com>
2025-08-12 18:52:34 +0900

    fix/redis INCR ms단위로 적용 & Synchronized 적용

--- src/main/java/com/anipick/backend/user/component/NicknameInitializer.java
+++ src/main/java/com/anipick/backend/user/component/NicknameInitializer.java
@@ -6,37 +6,42 @@ import com.anipick.backend.user.domain.LoginFormat;
 import com.anipick.backend.user.domain.UserDefaults;
 
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Component
 @RequiredArgsConstructor
 public class NicknameInitializer {
-    private static final int NICKNAME_THRESHOLD = 10;
+    private static final int NICKNAME_THRESHOLD = 1000;
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS");
+    private static final String COLON = ":";
 
     private final RedisTemplate<String, String> redisTemplate;
 
     public String generateNickname(LoginFormat loginFormat) {
-        String redisKey = UserDefaults.DEFAULT_NICKNAME_FORMAT_KEY + loginFormat;
+        String timeStamp = LocalDateTime.now().format(DATE_TIME_FORMATTER);
+        String loginFormatFirstLetter = loginFormat.toString().substring(0, 1);
+        String redisKey = UserDefaults.DEFAULT_NICKNAME_FORMAT_KEY + loginFormat + COLON + timeStamp;
         Long count = redisTemplate.opsForValue().increment(redisKey);
 
         if(count == null) {
             throw new CustomException(ErrorCode.INTERNAL_SERVER_ERROR);
         }
 
-        if(count >= NICKNAME_THRESHOLD) {
-            redisTemplate.delete(redisKey);
+        synchronized (this) {
+            if(count == 1L) {
+                redisTemplate.expire(redisKey, Duration.ofSeconds(10));
+            }
         }
 
-        long normalizedCount = count % NICKNAME_THRESHOLD;
-        String timeStamp = LocalDateTime.now().format(DATE_TIME_FORMATTER);
-        String loginFormatFirstLetter = loginFormat.toString().substring(0, 1);
-        String nickname = loginFormatFirstLetter + timeStamp + normalizedCount;
+        int tailCount = (int) ((count - 1L) % NICKNAME_THRESHOLD);
+        String tailNickname = String.format("%03d", tailCount);
 
-        return nickname;
+        return loginFormatFirstLetter + timeStamp + tailNickname;
     }
 }
