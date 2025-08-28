f8e36ffb274ac7e3e635cd239fa08dd7028aa852
ChoiSeoWoong <chldbfkr456@naver.com>
2025-08-28 22:05:58 +0900

    [#245] fix/HTTP prefix 추가

--- src/main/java/com/anipick/backend/image/domain/ImageDefaults.java
+++ src/main/java/com/anipick/backend/image/domain/ImageDefaults.java
@@ -4,6 +4,6 @@ import lombok.Getter;
 
 @Getter
 public class ImageDefaults {
-    public static final String IMAGE_ENDPOINT = "/api/image/";
+    public static final String IMAGE_ENDPOINT = "http://anipick.p-e.kr:8080/api/image/";
     public static final String DEFAULT_PROFILE_IMAGE_PATH = "static/image/default.jpg";
 }
