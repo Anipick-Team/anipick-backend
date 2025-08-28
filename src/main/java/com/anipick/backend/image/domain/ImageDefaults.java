ec5b922ae865bc5996c3e831dc7b7130f8ab462f
ChoiSeoWoong <chldbfkr456@naver.com>
2025-08-28 19:18:47 +0900

    [#245] fix/HTTP, HTTPS prefix 삭제

--- src/main/java/com/anipick/backend/image/domain/ImageDefaults.java
+++ src/main/java/com/anipick/backend/image/domain/ImageDefaults.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 
 @Getter
 public class ImageDefaults {
-    public static final String HTTP_IMAGE_ENDPOINT = "http://anipick.p-e.kr:8080/api/image/";
-    public static final String HTTPS_IMAGE_ENDPOINT = "https://anipick.p-e.kr/api/image/";
-    public static final String DEFAULT_PROFILE_IMAGE_PATH = "/image/default.jpg";
+    public static final String IMAGE_ENDPOINT = "/api/image/";
+    public static final String DEFAULT_PROFILE_IMAGE_PATH = "static/image/default.jpg";
 }
