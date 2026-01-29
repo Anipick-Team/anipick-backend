c07e64b2db859ac222e9014e2f3d540967e4aff6
yuuzinn <s110528@nate.com>
2026-01-29 20:13:46 +0900

    [#368] feat/정적 리소스 파일 캐싱 처리

--- src/main/java/com/anipick/backend/common/config/WebConfig.java
+++ src/main/java/com/anipick/backend/common/config/WebConfig.java
@@ -2,8 +2,11 @@ package com.anipick.backend.common.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -14,5 +17,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/profile/**")
                 .addResourceLocations("file:" + uploadDir);
+
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS).cachePublic());
     }
 }
