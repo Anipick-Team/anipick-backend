66f5b14118a121ecb9c6f93fa0aefb92c8488b60
yuuzinn <yyebo0219@gmail.com>
2026-04-12 20:08:34 +0900

    [#391] feat/LocalizationUtil 수동 번역(제목, 줄거리)추가

--- src/main/java/com/anipick/backend/common/util/LocalizationUtil.java
+++ src/main/java/com/anipick/backend/common/util/LocalizationUtil.java
@@ -2,13 +2,17 @@ package com.anipick.backend.common.util;
 
 public class LocalizationUtil {
     /**
+     * @param manualTitle 수동 번역
      * @param korTitle 번역
      * @param engTitle 영어
      * @param romajTitle 로마자
      * @param nativeTitle 원어
-     * @return 우선순위 번역-영어-로마자-원어 (전부 null일 경우, "제목 정보 없음" 리턴)
+     * @return 우선순위 매뉴얼 수동 번역 - 번역(kor)-영어-로마자-원어 (전부 null일 경우, "제목 정보 없음" 리턴)
      */
-    public static String pickTitle(String korTitle, String engTitle, String romajTitle, String nativeTitle) {
+    public static String pickTitle(String manualTitle, String korTitle, String engTitle, String romajTitle, String nativeTitle) {
+        if (manualTitle != null && !manualTitle.isBlank()) {
+            return manualTitle;
+        }
         if (korTitle != null && !korTitle.isBlank()) {
             return korTitle;
         }
@@ -22,6 +26,24 @@ public class LocalizationUtil {
             return nativeTitle;
         }
         return "제목 정보 없음";
+    }
+
+    /**
+     *
+     * @param descriptionManual
+     * @param descriptionKor
+     * @return
+     */
+    public static String pickDescription(String descriptionManual, String descriptionKor) {
+        if (descriptionManual != null && !descriptionManual.isBlank()) {
+            return descriptionManual;
+        }
+
+        if (descriptionKor != null && !descriptionKor.isBlank()) {
+            return descriptionKor;
+        }
+
+        return "소개 정보 없음";
     }
 
     /**
